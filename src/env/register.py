40b1b55c2ba7c36c45d57edd4e3ba5be9983a01c
Giacomo Pracucci <giacomo.pracucci8@gmail.com>
2023-09-08 21:04:38 +0200

    update

--- src/env/register.py
+++ src/env/register.py
@@ -1,8 +1,18 @@
-import sys
-sys.path.append('C:/Users/giaco/Desktop/tesi_git/src')
+import gymnasium as gym
+from env import TrafficManagementEnv
 from gymnasium.envs.registration import register
 
 register(
     id='TrafficManagement-v0',
     entry_point='env.env:TrafficManagementEnv',
+)
+
+from sheeprl.utils.env import make_env
+
+def create_custom_env(id, render_mode):
+    env = gym.make('TrafficManagement-v2', render_mode="rgb_array")
+    wrapped_env = TrafficManagementEnv(env)
+    return wrapped_env
+
+make_env("TrafficManagement-v0", create_custom_env, idx=0, capture_video=False)
 )
